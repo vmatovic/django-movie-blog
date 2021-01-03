ab346d4a4106cca1990596fa629ce7dc783ca69f
vmatovic <vmatovic3818rn@raf.rs>
2021-01-03 14:17:59 +0100

    Napravljena dinamicka stranica za svaki film

--- movieblog/baza/urls.py
+++ movieblog/baza/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "baza"
+
 urlpatterns = [
-	path('', views.home_page, name='home')
+	path('', views.home_page, name='home'),
+	path('movie/<int:id>', views.movie_page, name='movie_page')
 ]
--- movieblog/baza/views.py
+++ movieblog/baza/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from baza.models import Movie, Comment
 
 # Create your views here.
 def home_page(request):
 	popular_movies = Movie.objects.filter(revenue__gte=650000000)
 	return render(request, 'home.html', {'movies': popular_movies})
+
+def movie_page(request, id):
+	mov = get_object_or_404(Movie, pk=id)
+	return render(request, 'movie_page.html', {'movie': mov})
